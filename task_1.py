42f6b7389824cd986a0735eaa805c772aa536b28
Stepassshhhka <millisasneyksnk@gmail.com>
2024-11-20 20:28:08 +0300

    a

---
+++ task_1.py
@@ -0,0 +1,27 @@
+import random
+import timeit
+import matplotlib.pyplot as plt
+
+
+def func(arr):
+    even = [arr[i] for i in range(len(arr)) if i % 2 == 0]
+    odd = [arr[i] for i in range(len(arr)) if i % 2 != 0]
+    return even + odd
+#Сложность O(n)
+
+
+sizes = [10000, 20000, 30000, 40000, 50000]
+times = []
+for size in sizes:
+    arr = [random.randint(-size, size) for i in range(size)]
+    time = timeit.timeit('func(arr)', number=1000, globals=globals())
+    times.append(time)
+    print(f'Size: {size}, Time: {time}')
+
+plt.plot(sizes, times)
+
+plt.xlabel("Размер входных данных")
+plt.ylabel("Время выполнения (с)")
+plt.title("График времени выполнения функции")
+
+plt.show()
